d70e71007305f7062b1456fdd8b231d8ea728cc6
Ewa <ewa.rosset@softwire.com>
2021-11-03 16:07:34 +0000

    filter for the library page

---
+++ wwwroot/js/library.js
@@ -0,0 +1,18 @@
+function SearchByFunction(){
+    const search = document.getElementById("search").value.toLowerCase();
+    console.log(search);
+    
+    const children = document.getElementById("all-books").children
+    
+    let test = children[0].children[1];
+    console.log(test.innerHTML)
+    
+    for (i=0; i<children.length; i++){
+        let title = children[i].children[1].innerHTML.toLowerCase();
+        if (title.includes(search)){
+            children[i].style.display='flex'
+        } else {
+            children[i].style.display='none'
+        }
+    }
+}
