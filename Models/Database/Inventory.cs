b7cad800fd1ca69cbbb0431e6cc4fcd8b042688e
Charles Alexis <racharlesar@gmail.com>
2021-11-03 14:06:19 +0000

    More stuff for inventory page

--- Models/Database/Inventory.cs
+++ Models/Database/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Dapper;
 using Npgsql;
@@ -13,21 +14,33 @@ namespace Bookish.Models.Database
         {
             _Connection = Database.Connect();
         }
+
         public int copy_id { get; set; }
         public string title { get; set; }
         public int year_published { get; set; }
-        public string checked_out { get; set; }
+        public bool currently_out { get; set; }
+        public bool members_id { get; set; }
         public string author { get; set; }
         public string genre { get; set; }
 
         public IEnumerable<Inventory> GetAll()
         {
             var inventory = _Connection.Query<Inventory>(
-                $"SELECT genres.genre, books.title, books.year_published, authors.author, copies.id AS copy_id " +
+                $"SELECT " +
+                $"genres.genre, " +
+                $"books.title, " +
+                $"books.year_published, " +
+                $"authors.author, " +
+                $"copies.id AS copy_id, " +
+                $"(recently_borrowed.checked_out IS NOT NULL AND recently_borrowed.returned IS NULL) AS currently_out, " +
+                $"recently_borrowed.members_id " +
                 $"FROM genres " +
                 $"INNER JOIN books on genres.id = books.genre_id " +
                 $"INNER JOIN authors on authors.id = books.author_id " +
                 $"INNER JOIN copies on copies.book_id = books.id " +
+                $"LEFT JOIN (SELECT DISTINCT ON (copy_id) * from borrowed " +
+                $"ORDER BY copy_id, checked_out DESC) " +
+                $"recently_borrowed on recently_borrowed.copy_id = copies.id " +
                 $"Order by books.title;");
             _Connection.Close();
             return inventory;
