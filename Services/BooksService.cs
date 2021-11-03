43a489bbe6b987cfba4768763dc6d1f4cddb417e
Ewa <ewa.rosset@softwire.com>
2021-11-03 11:47:16 +0000

    Add the book service

---
+++ Services/BooksService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Bookish.Models.Database;
+using Dapper;
+using Npgsql;
+
+namespace Bookish.Services
+{
+    public interface IBooksService
+    {
+        IEnumerable<Book> GetAll();
+    }
+
+    public class BooksService : IBooksService
+    {
+        private const string ConnectionString = "Server=localhost;Port=5432;Database=bookish;Username=postgres;Password=bookish";
+
+        public IEnumerable<Book> GetAll()
+        {
+            using var connection = new NpgsqlConnection(ConnectionString);
+            return connection.Query<Book>("SELECT * FROM books");
+        }
+    }
+}
